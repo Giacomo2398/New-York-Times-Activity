7d2d8c695d2f27fa7c79cca88d160c0b5756a840
blakeahawkins <blakeahawkins@gmail.com>
2018-06-13 21:29:39 -0500

    This Javascript should work, except for the clear button.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,33 @@
 // Starting ajax branch
+var searchInput;
+var numRecords;
+var startYear;
+var endYear;
+
+var searchButton;
+var clearButton;
+
+function displayResults() {
+    var searchInput = $("#search-field").val().trim();
+    var numRecords = $("#number-field").val().trim();
+    var startYear = $("#start-field").val().trim();
+    var endYear = $("#end-field").val().trim();
+
+    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=adc9afb778e24487a40604627ff52071&q=" +
+        searchInput +
+        "&start_date=" +
+        startYear +
+        "0101&end_date=" +
+        endYear +
+        "0101";
+    
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+        console.log(JSON.stringify(response));
+        console.log(JSON.stringify(response.response.docs[1]));
+    })
+}
+
+$("#search-button").on("click", displayResults());
