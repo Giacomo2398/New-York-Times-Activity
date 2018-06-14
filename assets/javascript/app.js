228b84a439e46a6133dd00cb7e5e947e7b2fdc7b
blakeahawkins <blakeahawkins@gmail.com>
2018-06-13 22:54:45 -0500

    Switched order of jQuery link and Javascript link in HTML. Added search button ID.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,33 +1,48 @@
 // Starting ajax branch
-var searchInput;
-var numRecords;
-var startYear;
-var endYear;
+$(document).ready(function(){
+    var searchInput;
+    var numRecords;
+    var startYear;
+    var endYear;
 
-var searchButton;
-var clearButton;
+    var searchButton = $("#search-button");
+    var clearButton;
 
-function displayResults() {
-    var searchInput = $("#search-field").val().trim();
-    var numRecords = $("#number-field").val().trim();
-    var startYear = $("#start-field").val().trim();
-    var endYear = $("#end-field").val().trim();
+    function displayResults() {
+        var searchInput = $("#search-field").val().trim();
+        var numRecords = $("#number-field").val().trim();
+        var startYear = $("#start-field").val().trim();
+        var endYear = $("#end-field").val().trim();
 
-    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=adc9afb778e24487a40604627ff52071&q=" +
-        searchInput +
-        "&start_date=" +
-        startYear +
-        "0101&end_date=" +
-        endYear +
-        "0101";
-    
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function(response) {
-        console.log(JSON.stringify(response));
-        console.log(JSON.stringify(response.response.docs[1]));
-    })
-}
+        var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=adc9afb778e24487a40604627ff52071&q=" +
+            searchInput +
+            "&start_date=" +
+            startYear +
+            "0101&end_date=" +
+            endYear +
+            "0101";
+        
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function(response) {
+            console.log(JSON.stringify(response));
+            console.log(JSON.stringify(response.response.docs[1]));
+            var results = response.response.docs;
+            for(i = 0; i < numRecords; i++) {
+                var entry = $("<li>");
+                var link = $("<a>").attr("href", results[i].web_url)
+                link.attr("target", "_blank")
+                link.text(results[i].headline.main);
+                var author = $("<p>").text(results[i].byline.original);
+                entry.append(link);
+                entry.append(author);
+                $("#article-container").append(entry);
+            }
+        })
+    }
 
-$("#search-button").on("click", displayResults());
+    searchButton.on("click", function() {
+        displayResults()
+    });
+})
